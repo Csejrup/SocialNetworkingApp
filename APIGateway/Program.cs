febe2bc44994feb60355186c0df9813907021ec0
Casper  Sejrup <sejrup112@gmail.com>
2024-10-17 11:22:40 +0200

    remove ocelot from api gate way project.

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,17 +1,9 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
+
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add Ocelot
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-
-// Use Ocelot as Middleware
-await app.UseOcelot();
 
 app.Run();

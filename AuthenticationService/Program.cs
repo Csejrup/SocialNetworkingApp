7e0d2cc561dc28a3aa5925c68fe577604094ffee
CSejrup <55694118+Csejrup@users.noreply.github.com>
2024-12-02 16:36:38 +0100

    Update Program.cs

--- AuthenticationService/Program.cs
+++ AuthenticationService/Program.cs
@@ -1,22 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
-
-
-// Load settings
 var config = builder.Configuration.GetSection("Settings").Get<Settings>();
-
+// Auth config
 var jwtIssuer = config.JwtIssuer;
 var jwtKey = config.JwtKey;
-
-// Add services to the container.
+// Services
 builder.Services.AddSingleton(config);
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers(); // Ensure this line is present
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
